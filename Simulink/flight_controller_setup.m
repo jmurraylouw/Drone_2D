e4633f893c44c0da48300a8c4805dd8c8b2e2472
Murray <jmurraylouw@gmail.com>
2020-10-06 16:19:21 +0200

    added waypoints timeseries from worspace into simulink

--- Simulink/flight_controller_setup.m
+++ Simulink/flight_controller_setup.m
@@ -15,6 +15,33 @@ C_Dz = 0.2; % Damping coef. of drone in z direction (f = cy*zdot)
 rho = 1.225; % Air density (kg/m^3)
 tau = 0.07; % Motor time constant
 
-% Mixing Matrix.
+% Mixing Matrix
 MM = [-1, 1;
        1, 1]; % [T1; T2] = MM*[delta_E; delta_T]
+   
+% Way points
+num_waypoints = 10; % Number of waypoints included in command
+waypoints = table('Size', [num_waypoints, 3], 'VariableTypes', ["double", "double", "double"]);
+waypoints.Properties.VariableNames = {'point_time', 'x_coord', 'z_coord'};
+waypoints(1,:) = table(0, 0, -5); % Initial point
+point_time_interval = 10; % (s) time interval between points
+x_min = -10; x_max = 10; % minimum and maximum coordinates for waypoints
+z_min = -25; z_max = -5;
+
+rng(0); % Initialise random number generator for repeatability
+for i = 1:2:num_waypoints*2+1
+    x_coord    = (x_max - x_min).*rand() + x_min; % x coordinate of next waypoint
+    z_coord    = (z_max - z_min).*rand() + z_min; % z coordinate of next waypoint
+    waypoints(i+1,:) = table(point_time_interval*(i),   x_coord, z_coord);
+    waypoints(i+2,:) = table(point_time_interval*(i+1), x_coord, z_coord);
+end
+waypoints
+waypoints_ts = timeseries([waypoints.x_coord, waypoints.z_coord], waypoints.point_time); % timeseries object for import into simulink
+waypoints_ts = setinterpmethod(waypoints_ts,'zoh'); % Set ZOH as interpolation method
+
+% figure;
+% title('Waypoint route')
+% plot(waypoints.x_coord, waypoints.z_coord)
+% xlabel('x');
+% ylabel('z');
+
